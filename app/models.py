ea649ba626053e30f752f7f943ec22c0e761e35e
Aral Can Kaymaz <aral.kaymaz@fam.ch>
2023-08-03 17:43:37 +0200

    Aral - follower model

--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def load_user(id):
     return User.query.get(int(id))
 
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -17,6 +23,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
     
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
@@ -40,23 +52,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {} createt at {}>'.format(self.body, self.timestamp)
-
-    def follow(self, user):
-        if not self.is_following(user) and self != user:
-            return Follow(follower_id=self.id, followed_id=user.id)
-
-    def unfollow(self, user):
-        if self.is_following(user) and self != user:
-            return Follow(follower_id=self.id, followed_id=user.id)
-
-    def is_following(self, user):
-        return self.followed.filter(
-            followers.c.followed_id == user.id).count() > 0
-    
-class Follow(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    follower_id = db.Column(db.Integer, db.ForeignKey('user.id')) # follower_id is the user who is doing the following
-    followed_id = db.Column(db.Integer, db.ForeignKey('user.id')) # followed_id is the user who is being followed
-    
-    def __repr__(self):
-        return '<Follow {} is following {}>'.format(self.follower_id, self.followed_id)
